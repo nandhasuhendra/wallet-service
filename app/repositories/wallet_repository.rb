99f6c5cd3e3963749643d829f49797bb533863a3
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-18 20:16:14 +0800

    feat: Create a new function for find_by_id and lock the row

--- app/repositories/wallet_repository.rb
+++ app/repositories/wallet_repository.rb
@@ -1,22 +1,28 @@
 class WalletRepository
-  def self.owner_wallets(owner)
-    Wallet.includes(:owner).where(owner: owner).order(created_at: :asc)
-  end
+  class << self
+    def owner_wallets(owner)
+      Wallet.includes(:owner).where(owner: owner).order(created_at: :asc)
+    end
 
-  def self.create_default_wallet(owner)
-    Wallet.create!(owner: owner, name: Wallet::DEFAULT_WALLET_NAME, balance: 0.0, primary: true)
-  end
+    def create_default_wallet(owner)
+      Wallet.create!(owner: owner, name: Wallet::DEFAULT_WALLET_NAME, balance: 0.0, primary: true)
+    end
 
-  def self.create_wallet(owner, name, balance)
-    wallet = wallet = Wallet.new(name: name, owner: owner, balance: balance)
-    wallet.save
-  end
+    def create_wallet(owner, name, balance)
+      wallet = wallet = Wallet.new(name: name, owner: owner, balance: balance)
+      wallet.save
+    end
 
-  def self.find_by_owner(owner)
-    Wallet.find_by(owner: owner)
-  end
+    def find_by_owner(owner)
+      Wallet.find_by(owner: owner)
+    end
 
-  def self.find_by_id_and_owner(wallet_id, owner)
-    Wallet.find_by(id: wallet_id, owner: owner)
+    def find_by_id_and_owner(wallet_id, owner)
+      Wallet.find_by(id: wallet_id, owner: owner)
+    end
+
+    def find_by_id_and_lock(wallet_id, owner)
+      Wallet.where(id: wallet_id, owner: owner).lock("FOR UPDATE").last
+    end
   end
 end
