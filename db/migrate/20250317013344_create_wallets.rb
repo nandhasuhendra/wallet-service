969d3ffb87c7141bba37908aeaa855bcf32ad3bc
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-18 09:22:23 +0800

    fix: Add column to mark wallet as primary

--- db/migrate/20250317013344_create_wallets.rb
+++ db/migrate/20250317013344_create_wallets.rb
@@ -3,6 +3,7 @@ class CreateWallets < ActiveRecord::Migration[8.0]
     create_table :wallets, id: false do |t|
       t.bigint :id, primary_key: true, null: false
       t.string :name, null: false
+      t.boolean :primary, default: false, null: false
       t.decimal :balance, precision: 10, scale: 2, default: 0.0, null: false
       t.references :owner, polymorphic: true, null: false
       t.datetime :deleted_at
